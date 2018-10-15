b413c0cda4b50140c7234a5ba978d5d4ee0f8bcd
Ieva Desktop <ieva.cernyte@gmail.com>
2018-10-16 00:07:01 +0100

    Attempt at TextMesh Pro

---
+++ Text Editor/Assets/Scripts/FingerPointer.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FingerPointer : MonoBehaviour {
+
+    public float movement_size = 1.0f;
+
+	void Start ()
+    {
+		
+	}
+	
+	void Update ()
+    {
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            Vector3 position = this.transform.position;
+            position.x -= movement_size;
+            this.transform.position = position;
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            Vector3 position = this.transform.position;
+            position.x += movement_size;
+            this.transform.position = position;
+        }
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            Vector3 position = this.transform.position;
+            position.y += movement_size;
+            this.transform.position = position;
+        }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            Vector3 position = this.transform.position;
+            position.y -= movement_size;
+            this.transform.position = position;
+        }
+        if (Input.GetKeyDown(KeyCode.LeftControl))
+        {
+            Debug.Log("Down");
+            Debug.Log(Camera.main.WorldToScreenPoint(transform.position));
+            //Vector2 position = Camera.main.WorldToScreenPoint(transform.position);
+            //MouseOperations.MouseEvent(MouseOperations.MouseEventFlags.LeftDown, position);
+        }
+        //if (Input.GetKeyUp(KeyCode.LeftControl))
+        //{
+        //    Debug.Log("Up");
+        //    MouseOperations.MouseEvent(MouseOperations.MouseEventFlags.LeftUp);
+        //}
+    }
+}
