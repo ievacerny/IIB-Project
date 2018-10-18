9725aa0b79910886efb5031e492e0659d7a3b917
Ieva C <ieva.cernyte@gmail.com>
2018-10-18 17:11:26 +0100

    Mapping between coordinates and indices

--- Text Editor/Assets/Scripts/ActivePage.cs
+++ Text Editor/Assets/Scripts/ActivePage.cs
@@ -9,8 +9,15 @@ public class ActivePage : MonoBehaviour {
 
     public GameObject frame_quad;
     private Renderer frame_quad_rend;
+    private Transform cursor_transform;
 
     private bool cursor_on_page = false;
+
+    private float margin = 0.00575f;
+    private float width = 0.0186f;
+    private float height = 0.0643f;
+
+    ModelViewMapping modelview;
 
     // Use this for initialization
     void Start ()
@@ -27,6 +34,8 @@ public class ActivePage : MonoBehaviour {
         Color new_color = frame_quad_rend.material.color;
         new_color.a = 0f;
         frame_quad_rend.material.color = new_color;
+
+        modelview = new ModelViewMapping(margin, width, height);
     }
 	
 	// Update is called once per frame
@@ -53,14 +62,27 @@ public class ActivePage : MonoBehaviour {
             new_color.a = 0f;
             frame_quad_rend.material.color = new_color;
         }
-		
-	}
+
+        if (cursor_transform != null)
+        {
+            Vector3 coords = transform.InverseTransformPoint(cursor_transform.position);
+            if (coords.x <= 0.5f && coords.x >= -0.5f && coords.y <= 0.5 && coords.y >= -0.5f)
+            {
+                var ind = modelview.CoordsToIndex(coords);
+                var coords2 = modelview.IndexToCoords(ind);
+                Debug.Log(string.Format("Real {0}, new {1}; row {2}, col {3}", coords.ToString("F4"), coords2.ToString("F4"), ind.row, ind.col));
+            }
+        }
+        
+
+    }
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.name == "FingerPoint")
         {
             cursor_on_page = true;
+            cursor_transform = other.transform;
 
             if (!inEditMode)
             {
@@ -76,6 +98,7 @@ public class ActivePage : MonoBehaviour {
         if (other.gameObject.name == "FingerPoint")
         {
             cursor_on_page = false;
+            cursor_transform = null;
 
             if (!inEditMode)
             {
---
+++ Text Editor/Assets/Scripts/ModelViewMapping.cs
@@ -0,0 +1,104 @@
+﻿using UnityEngine;
+
+public class ModelViewMapping {
+
+    private float margin;
+    private float width;
+    private float height;
+
+    Vector3 end = new Vector3(0.4818f, -0.4326f, 0f);
+    Vector3 beg = new Vector3(-0.4874f, 0.4062f, 0f);
+
+    public ModelViewMapping(float margin, float width, float height)
+    {
+        this.margin = margin;
+        this.width = width;
+        this.height = height;
+    }
+
+    public Character CoordsToIndex(Vector3 coords)
+    {
+        var indices = new Character();
+
+        indices.row = (int) ((coords.y - 0.5f) / -height);
+        indices.col = (int) ((coords.x + (0.5f - margin)) / width);
+        //Debug.Log(string.Format("Row {0}, column {1}", indices.row, indices.col));
+
+        return indices;
+    }
+
+    public Vector3 IndexToCoords(Character indices)
+    {
+        var coords = new Vector3();
+
+        coords.y = ((float)indices.row + 0.5f) * -height + 0.5f;
+        coords.x = ((float)indices.col + 0.5f) * width - (0.5f - margin);
+        coords.z = 0f;
+
+        return coords;
+    }
+
+
+
+
+    // TODO: automatically detect model size based on canvas and font size
+    private char[,] model;
+    public int[] model_size = new int[] { 15, 53 };
+
+    void BuildModel(string text, int[] model_size)
+    {
+        model = new char[model_size[0], model_size[1]];
+
+        string[] lines = text.Split(new char[] { '\n' });
+
+        // TODO: Assume for now, that text will always be within model size. Fix later
+        for (int i = 0; i < lines.Length; i++)
+        {
+            for (int j = 0; j < lines[i].Length; j++)
+            {
+                model[i, j] = lines[i][j];
+            }
+        }
+    }
+
+    // Function used to optimise the width and height of the characters, and the margin of the page
+    void OptimiseParams()
+    {
+        float best_margin = 100f;
+        float best_width = 100f;
+        float min_distance = 100f;
+        for (margin = 0f; margin <= 0.007f; margin += 0.00001f)
+        {
+            for (width = 0.01f; width <= 0.06f; width += 0.0001f)
+            {
+                var end_i = CoordsToIndex(end);
+                var end_c = IndexToCoords(end_i);
+
+                var beg_i = CoordsToIndex(beg);
+                var beg_c = IndexToCoords(beg_i);
+
+                if ((Mathf.Abs(end_c.x - end.x) + Mathf.Abs(beg_c.x - beg.x) < min_distance) &&
+                    end_i.col == 52 && beg_i.col == 0)
+                {
+                    min_distance = Mathf.Abs(end_c.y - end.y) + Mathf.Abs(beg_c.y - beg.y);
+                    best_margin = margin;
+                    best_width = width;
+                }
+            }
+        }
+
+        Debug.Log(min_distance);
+        Debug.Log(best_width);
+        Debug.Log(best_margin);
+        margin = best_margin;
+        width = best_width;
+    }
+
+}
+
+
+public class Character
+{
+    public int row;
+    public int col;
+}
